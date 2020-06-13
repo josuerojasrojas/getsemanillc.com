2b777f58257c5c39b23e2b8f380d3c0d8f6778a6
josue rojas <josuerojas.rojas@gmail.com>
2020-06-13 18:36:14 -0500

    Updates

---
+++ precache-manifest.55976c7f32c09b520cee0576d745e552.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8c1714e06c151e8e20a5e7496eefb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1b7f12464afad2e82b8",
+    "url": "/static/css/main.8c9a66fc.chunk.css"
+  },
+  {
+    "revision": "ade5b12849d9f4dd6e14",
+    "url": "/static/js/2.44af2885.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.44af2885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b7f12464afad2e82b8",
+    "url": "/static/js/main.f76d47ed.chunk.js"
+  },
+  {
+    "revision": "5171423ae72b6b1de8cb",
+    "url": "/static/js/runtime-main.09d0fcc3.js"
+  },
+  {
+    "revision": "e5bb189b93ac9a25bb5310e0e0d8fd9c",
+    "url": "/static/media/blob.e5bb189b.svg"
+  },
+  {
+    "revision": "3efca691cb4b06d91be4b4d108dd854a",
+    "url": "/static/media/image.3efca691.svg"
+  },
+  {
+    "revision": "19f4810cf58bf4d58a76f23a73ae41b8",
+    "url": "/static/media/option_0.19f4810c.png"
+  },
+  {
+    "revision": "4475b8ede7e316cd3aba833341b1f2f7",
+    "url": "/static/media/option_1.4475b8ed.png"
+  },
+  {
+    "revision": "e7746265c88141758015507b124807cf",
+    "url": "/static/media/option_3.e7746265.png"
+  },
+  {
+    "revision": "769704c0d7bda72153e9c7962c69158d",
+    "url": "/static/media/option_4.769704c0.png"
+  }
+]);
